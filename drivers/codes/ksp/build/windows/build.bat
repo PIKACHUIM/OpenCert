fe255279faef8d4fabbdd7b2403cf83dd5eccc63
pikachuim <pikachuim@qq.com>
2026-05-28 22:14:34 +0800

    fix(driver): fix window not show issue

---
+++ drivers/codes/ksp/build/windows/build.bat
@@ -0,0 +1,39 @@
+@rem Initialize build environment of Visual Studio 2022 Community/Professional/Enterprise
+@set tools=
+@set tmptools="c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
+@if exist %tmptools% set tools=%tmptools%
+@set tmptools="c:\Program Files\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
+@if exist %tmptools% if not defined tools set tools=%tmptools%
+@if not defined tools goto :error
+
+@rem Build for x86 platform
+@setlocal
+@echo on
+call %tools% x86
+@echo on
+msbuild OpenCertKSP.sln /p:Configuration=Release /p:Platform=Win32 /target:Clean || goto :error
+msbuild OpenCertKSP.sln /p:Configuration=Release /p:Platform=Win32 /target:Build || goto :error
+copy .\Win32\Release\OpenCertKSP_x86.dll . || goto :error
+@endlocal
+
+@rem Build for x64 platform
+@setlocal
+@echo on
+call %tools% x64
+@echo on
+msbuild OpenCertKSP.sln /p:Configuration=Release /p:Platform=x64 /target:Clean || goto :error
+msbuild OpenCertKSP.sln /p:Configuration=Release /p:Platform=x64 /target:Build || goto :error
+copy .\x64\Release\OpenCertKSP_x64.dll . || goto :error
+@endlocal
+
+@echo *** BUILD SUCCESSFUL ***
+@exit /b %errorlevel%
+
+:error
+@echo *** BUILD FAILED ***
+@endlocal
+@exit /b %errorlevel%
